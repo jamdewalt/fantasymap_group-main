943283cbf8f3f5f975a9890c1c76f27e37f6dffa
Eli-Sam <51330314+Eli-Sam@users.noreply.github.com>
2022-04-26 15:56:06 -0500

    Organized and Fixed
    Cleaned up and added popups.

--- webmapping_final_group-main/js/main.js
+++ webmapping_final_group-main/js/main.js
@@ -1,31 +1,11 @@
 //Here we declare map variable globally so all functions have access
 var map;
-var minValue;
-var dataStats = {};
-function StateStyle(feature) {
-  return {
-    fillColor: feature.properties.Color,
-    color: "#000",
-    weight: 1,
-    opacity: 1,
-    fillOpacity: 0.8
-  };
-}
-var citystyle = {
-  radius: 3,
-  fillColor: "#000",
-  color: "#000",
-  weight: 1,
-  opacity: 1,
-  fillOpacity: 1
-};
-var riverstyle = {
-  fillColor: "#3944bc",
-  color: "#3944bc",
-  weight: 1,
-  opacity: 1,
-  fillOpacity: 1
-};
+var biomesLayer;
+var stateLayer;
+var riverLayer;
+var citiesLayer;
+var routes;
+
 //Create map function
 function createMap(){
 //Create the map
@@ -39,24 +19,83 @@ L.tileLayer('', {
         'attribution': 'Map data &copy; OpenStreetMap contributors'
 }).addTo(map);
 
-baselayers(map)
+states(map)
+cities(map)
+rivers(map)
 addablelayers(map)
 };
-function baselayers(map){
-  var stateLayer = new L.GeoJSON.AJAX("data/Atlantis_states.geojson",{
-      style: StateStyle
-  }).addTo(map);
 
+//River Section
+function rivers(map){
+  var riverstyle = {
+    fillColor: "#3944bc",
+    color: "#3944bc",
+    weight: 1,
+    opacity: 1,
+    fillOpacity: 1
+  };
   var riverLayer = new L.GeoJSON.AJAX("data/Atlantis_rivers.geojson",{
       style: riverstyle
   }).addTo(map);
+}
 
-  var citiesLayer = new L.GeoJSON.AJAX("data/Atlantis_cities.geojson", {pointToLayer: function (feature, latlng) {
-          return L.circleMarker(latlng, citystyle);
-      }
+// State Section
+function states(map){
+  function StateStyle(feature) {
+    return {
+      fillColor: feature.properties.Color,
+      color: "#000",
+      weight: 1,
+      opacity: 1,
+      fillOpacity: 0.8
+    };
+  }
+  var stateLayer = new L.GeoJSON.AJAX("data/Atlantis_states.geojson",{style: StateStyle
+    ,onEachFeature: function (feature, layer) {
+        layer.bindTooltip(feature.properties.State);
+    }
   }).addTo(map);
 };
 
+//City Section
+function cities(map){
+  var citiesLayer = new L.GeoJSON.AJAX("data/Atlantis_cities.geojson", {pointToLayer: function(feature, latlng){
+            return citypointToLayer(feature, latlng);
+             }
+      }).addTo(map);
+  };
+
+function cityPopupContent(properties){
+  //Using object oriented programming to create different parts of the popup
+  this.properties = properties;
+  this.cityname = this.properties["Burg"]
+  this.population = this.properties["Population"]
+  //Creates the format for the popup
+  this.formatted = "<p><b>City:</b> " + this.cityname + "</p><p><b>Population:</b>" + this.population + "</p>" ;
+};
+
+function citypointToLayer(feature, latlng){
+    var citystyle = {
+      radius: 3,
+      fillColor: "#000",
+      color: "#000",
+      weight: 1,
+      opacity: 1,
+      fillOpacity: 1
+    };
+    var layer = L.circleMarker(latlng, citystyle);
+    //Build popup content string
+    var citypopupContent = new cityPopupContent(feature.properties);
+    //Bind the popup to the circle marker
+    layer.bindPopup(citypopupContent.formatted, {
+        offset: new L.Point(0,-5)
+    });
+
+    //Return the circle marker to the L.geoJson pointToLayer option
+    return layer;
+};
+
+//Overlay Layers
 function addablelayers(map){
   var biomesLayer = new L.GeoJSON.AJAX("data/Atlantis_biomes.geojson");
   var routes = new L.GeoJSON.AJAX("data/Atlantis_routes.geojson");
@@ -66,5 +105,6 @@ function addablelayers(map){
   };
   L.control.layers(null, mixed).addTo(map);
 };
+
 
 document.addEventListener('DOMContentLoaded',createMap)
