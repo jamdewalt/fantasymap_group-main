187504e57d3116af561b93ce96705093c39e86e1
Eli-Sam <51330314+Eli-Sam@users.noreply.github.com>
2022-04-26 11:59:35 -0500

    Visualizing
    Geojsons show up.

--- webmapping_final_group-main/js/main.js
+++ webmapping_final_group-main/js/main.js
@@ -5,53 +5,69 @@ var dataStats = {};
 
 //Create map function
 function createMap(){
-
+/*
   mapboxgl.accessToken = 'pk.eyJ1IjoiZXNhbTIwIiwiYSI6ImNsMW8xZGwzNDBkNWczY210djNvYml1OXUifQ.L_PC7U15MvJfggwW1XvNlg';
-  const map = new mapboxgl.Map({
+  map = new mapboxgl.Map({
       container: 'map', // container ID
       style: 'mapbox://styles/esam20/cl288aokd003z14nwn3tjmy6u', // style URL
       center: [-74.5, 40], // starting position [lng, lat]
       zoom: 9 // starting zoom
   });
-  //Call getData function
-  getData(map);
+*/
+//Create the map
+map = L.map('map', {
+  center: [9, 10],
+  zoom: 2
+});
+
+//Add OSM base tilelayer in this case watercolor
+L.tileLayer('', {
+        'attribution': 'Map data &copy; OpenStreetMap contributors'
+}).addTo(map);
+
+var stateLayer = new L.GeoJSON.AJAX("data/Atlantis_states.geojson");
+stateLayer.addTo(map);
+var riverLayer = new L.GeoJSON.AJAX("data/Atlantis_rivers.geojson");
+riverLayer.addTo(map);
+var citiesLayer = new L.GeoJSON.AJAX("data/Atlantis_cities.geojson");
+citiesLayer.addTo(map);
+var biomesLayer = new L.GeoJSON.AJAX("data/Atlantis_biomes.geojson");
+biomesLayer.addTo(map);
+
 };
+
+/*
 //Import GeoJSON data
 function getData(map){
        //Load the data
-       fetch("data/Atlantis.geojson")
+       fetch("data/Atlantis_cities.geojson")
            .then(function(response){
                return response.json();
            })
            .then(function(json){
                 //Create an attributes array and call other functions
                //calcStats(json);
-               var attributes = processData(json);
+               //var attributes = processData(json);
                //minValue = calculateMinValue(json);
-              // createPropSymbols(json, attributes);
+               createPropSymbols(json);
                //createSequenceControls(attributes);
                //createLegend(attributes);
            })
    };
-   function processData(data){
-       //Empty array to hold attributes
-       var attributes = [];
+   function pointToLayer(latlng){
+       //Create circle marker layer
+       var layer = L.marker(latlng);
 
-       //Properties of the first feature in the dataset
-       var properties = data.features[0].properties;
-
-       //Push each attribute name into attributes array
-       for (var attribute in properties){
-           //Only take attributes with Tourist values
-           if (attribute.indexOf("Tour") > -1){
-               attributes.push(attribute);
-           };
-       };
-
-       //Check result
-       console.log(attributes);
-
-       return attributes;
+       //Return the circle marker to the L.geoJson pointToLayer option
+       return layer;
    };
-
+   function createPropSymbols(data){
+       //create a Leaflet GeoJSON layer and add it to the map
+       L.geoJson(data, {
+         pointToLayer: function(latlng){
+             return pointToLayer(latlng);
+              }
+       }).addTo(map);
+   };
+*/
 document.addEventListener('DOMContentLoaded',createMap)
