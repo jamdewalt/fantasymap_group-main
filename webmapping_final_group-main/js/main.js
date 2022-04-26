1ed7e154694ddf2d8b088677051e7d878f94c6e4
Eli-Sam <51330314+Eli-Sam@users.noreply.github.com>
2022-04-26 12:39:31 -0500

    Mapping
    Now with Checkbox layers

--- webmapping_final_group-main/js/main.js
+++ webmapping_final_group-main/js/main.js
@@ -2,22 +2,28 @@
 var map;
 var minValue;
 var dataStats = {};
+var statestyle = {
+    fillColor: "#68bd56",
+    color: "#000",
+    weight: 1,
+    opacity: 1,
+    fillOpacity: 0.8
+};
+var citystyle = {
+  radius: 3,
+  fillColor: "#000",
+  color: "#000",
+  weight: 1,
+  opacity: 1,
+  fillOpacity: 1
+};
 
 //Create map function
 function createMap(){
-/*
-  mapboxgl.accessToken = 'pk.eyJ1IjoiZXNhbTIwIiwiYSI6ImNsMW8xZGwzNDBkNWczY210djNvYml1OXUifQ.L_PC7U15MvJfggwW1XvNlg';
-  map = new mapboxgl.Map({
-      container: 'map', // container ID
-      style: 'mapbox://styles/esam20/cl288aokd003z14nwn3tjmy6u', // style URL
-      center: [-74.5, 40], // starting position [lng, lat]
-      zoom: 9 // starting zoom
-  });
-*/
 //Create the map
 map = L.map('map', {
-  center: [9, 10],
-  zoom: 2
+  center: [10, 10],
+  zoom: 4
 });
 
 //Add OSM base tilelayer in this case watercolor
@@ -25,49 +31,23 @@ L.tileLayer('', {
         'attribution': 'Map data &copy; OpenStreetMap contributors'
 }).addTo(map);
 
-var stateLayer = new L.GeoJSON.AJAX("data/Atlantis_states.geojson");
-stateLayer.addTo(map);
+var stateLayer = new L.GeoJSON.AJAX("data/Atlantis_states.geojson",{
+    style: statestyle
+}).addTo(map);
 var riverLayer = new L.GeoJSON.AJAX("data/Atlantis_rivers.geojson");
-riverLayer.addTo(map);
-var citiesLayer = new L.GeoJSON.AJAX("data/Atlantis_cities.geojson");
-citiesLayer.addTo(map);
+//riverLayer.addTo(map);
+var citiesLayer = new L.GeoJSON.AJAX("data/Atlantis_cities.geojson", {pointToLayer: function (feature, latlng) {
+        return L.circleMarker(latlng, citystyle);
+    }
+}).addTo(map);
 var biomesLayer = new L.GeoJSON.AJAX("data/Atlantis_biomes.geojson");
-biomesLayer.addTo(map);
-
+var routes = new L.GeoJSON.AJAX("data/Atlantis_routes.geojson");
+var mixed = {
+  "Biomes": biomesLayer, // BaseMaps
+  "Travel Routes": routes,
+};
+L.control.layers(null, mixed).addTo(map);
 };
 
-/*
-//Import GeoJSON data
-function getData(map){
-       //Load the data
-       fetch("data/Atlantis_cities.geojson")
-           .then(function(response){
-               return response.json();
-           })
-           .then(function(json){
-                //Create an attributes array and call other functions
-               //calcStats(json);
-               //var attributes = processData(json);
-               //minValue = calculateMinValue(json);
-               createPropSymbols(json);
-               //createSequenceControls(attributes);
-               //createLegend(attributes);
-           })
-   };
-   function pointToLayer(latlng){
-       //Create circle marker layer
-       var layer = L.marker(latlng);
 
-       //Return the circle marker to the L.geoJson pointToLayer option
-       return layer;
-   };
-   function createPropSymbols(data){
-       //create a Leaflet GeoJSON layer and add it to the map
-       L.geoJson(data, {
-         pointToLayer: function(latlng){
-             return pointToLayer(latlng);
-              }
-       }).addTo(map);
-   };
-*/
 document.addEventListener('DOMContentLoaded',createMap)
