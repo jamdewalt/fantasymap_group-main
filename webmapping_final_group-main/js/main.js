96d17f1caaab44d6233f2e8fa9f1b484a9af1e2c
jamdewalt <98490986+jamdewalt@users.noreply.github.com>
2022-05-04 10:10:43 -0500

    Update main.js

--- webmapping_final_group-main/js/main.js
+++ webmapping_final_group-main/js/main.js
@@ -314,6 +314,7 @@ function autocomplete(inp, arr) {
 /*execute a function when someone clicks in the document:*/
 document.addEventListener("click", function (e) {
     closeAllLists(e.target);
+    new L.Marker([e.latlng.lat, e.latlng.lng]).addTo(map);
 });
 }
 
